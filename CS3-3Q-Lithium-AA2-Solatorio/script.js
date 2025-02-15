17908c64e8105e143aa81ced5ce53c30b895ea09
GussesFringe <lmmsolatorio22@src.pshs.edu.ph>
2025-02-15 15:57:51 +0800

    Add files via upload

---
+++ CS3-3Q-Lithium-AA2-Solatorio/script.js
@@ -0,0 +1,15 @@
+function newFunc(){
+    let initialPopulation = parseFloat(prompt("Enter the initial population: "));
+    let rateOfGrowth = parseFloat(prompt("Enter the rate of growth: "));
+    let time = parseFloat(prompt("Enter the time (In Hours): "));
+
+    let exponentialPopulationGrowth = Math.round(initialPopulation * Math.pow(Math.E,rateOfGrowth*time));
+
+    let regionOfMonster = prompt("Enter the region/location of the monster: ");
+    let nameOfMonster = prompt("Enter the name of the monster: ");
+
+    let monsterInfo = (regionOfMonster + " " + nameOfMonster).toUpperCase();
+
+    document.getElementById('result').innerHTML = "After " + time + " hours," + " the population of " + monsterInfo + " has risen to " + exponentialPopulationGrowth;
+
+}
